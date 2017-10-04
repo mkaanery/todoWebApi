bb01ff6379e22aba6de719cdb14a4b0c657f1fee
mkaanery <mahmutkaan.eryilmaz@agu.edu.tr>
2017-10-04 08:19:52 +0300

    modified TodoItem to add properties

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,9 +3,8 @@ namespace todoWebApi.Models
 {
     public class TodoItem
     {
-        
-        public TodoItem()
-        {
-        }
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public bool IsComplete { get; set; }
     }
 }
